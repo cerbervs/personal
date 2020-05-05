d7ed608b178f60897448daddc3fc1b6a059baf56
Mark Bailey <mbbailey96@gmail.com>
2020-05-05 04:13:31 -0400

    fixed images on mobile. probs done until "about" update

--- src/cljs/personal/websites.cljs
+++ src/cljs/personal/websites.cljs
@@ -8,12 +8,12 @@
      [:br]
      [:div.grid.grid-rows-flow
       [:div.flex.flex-none.grid.grid-cols-3.w-full
-       [:img.website-img.lg:max-w-md.zoom {:src "img/lal1.png"}]
-       [:img.website-img.lg:max-w-md.zoom {:src "img/lal2.png"}]
-       [:img.website-img.lg:max-w-md.zoom {:src "img/lal3.png"}]]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal1.png"}]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal2.png"}]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal3.png"}]]
       [:div.flex.flex-none.grid.grid-cols-3.w-full
-       [:img.website-img.max-w-md.zoom {:src "img/lal4.png"}]
-       [:img.website-img.lg:max-w-md.zoom {:src "img/lal5.png"}]
-       [:img.website-img.lg:max-w-md.zoom {:src "img/lal6.png"}]]]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal4.png"}]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal5.png"}]
+       [:img.website-img.lg:w-auto.lg:max-w-md.zoom {:src "img/lal6.png"}]]]
      [:br]
      [:p.text-md "Created for Kira, by hand, using CLJS and ReactJS together."]]))
