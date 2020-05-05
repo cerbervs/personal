bffb5ddeb02e05028080204b87156971a5f93389
Mark Bailey <mbbailey96@gmail.com>
2020-05-05 03:52:50 -0400

    i lied about final commit

--- src/cljs/personal/websites.cljs
+++ src/cljs/personal/websites.cljs
@@ -7,13 +7,13 @@
                  :target "_blank"} "Love and Labradorite"]
      [:br]
      [:div.grid.grid-rows-flow
-      [:div.grid.grid-cols-3.w-full
-       [:img.website-img.zoom {:src "img/lal1.png"}]
-       [:img.website-img.zoom {:src "img/lal2.png"}]
-       [:img.website-img.zoom {:src "img/lal3.png"}]]
-      [:div.grid.grid-cols-3.w-full
-       [:img.website-img.zoom {:src "img/lal4.png"}]
-       [:img.website-img.zoom {:src "img/lal5.png"}]
-       [:img.website-img.zoom {:src "img/lal6.png"}]]]
+      [:div.flex.flex-none.grid.grid-cols-3.w-full
+       [:img.website-img.lg:max-w-md.zoom {:src "img/lal1.png"}]
+       [:img.website-img.lg:max-w-md.zoom {:src "img/lal2.png"}]
+       [:img.website-img.lg:max-w-md.zoom {:src "img/lal3.png"}]]
+      [:div.flex.flex-none.grid.grid-cols-3.w-full
+       [:img.website-img.max-w-md.zoom {:src "img/lal4.png"}]
+       [:img.website-img.lg:max-w-md.zoom {:src "img/lal5.png"}]
+       [:img.website-img.lg:max-w-md.zoom {:src "img/lal6.png"}]]]
      [:br]
      [:p.text-md "Created for Kira, by hand, using CLJS and ReactJS together."]]))
